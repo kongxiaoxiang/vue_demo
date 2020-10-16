158eeb61d960c4a8e6c468e385226a13855b941b
mac <mac@192.168.2.2>
2020-10-16 10:46:43 +0800

    更新

---
+++ src-HelloWorld/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="test">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        msg:'Hello World Vue!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .test{
+    color: aquamarine;
+    font-size: 40px;
+  }
+</style>
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Add/>
+    <List :comments='comments'/>
+  </div>
+</template>
+
+<script>
+  import Add from './components/Add'
+  import List from './components/List'
+  export default {
+    data(){
+      return {
+        comments:[
+          {name:'xiaozhang',content:'vue is good'},
+          {name:'xiaoxiang',content:'vue is easy'},
+          {name:'xiaozhao',content:'vue just so so'}
+        ]
+      }
+    },
+    components:{
+      Add,
+      List
+    }
+  }
+</script>
+
+<style>
+
+</style>
