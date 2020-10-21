052ed09a26cad51f970fc6dbb42b43036438f1a9
mac <mac@192.168.2.3>
2020-10-21 21:39:48 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+    <Header :addItem='addItem'/>
+    <List :todos='todos'/>
+    <Footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import List from './components/List'
+  import Footer from './components/Footer'
+  export default {
+    data(){
+      return {
+        todos:[
+          {title:'eat',complete:false},
+          {title:'sleeping',complete:false},
+          {title:'lol',complete:true}
+        ]
+      }
+    },
+    components:{
+      Header,
+      List,
+      Footer
+    },
+    methods:{
+      addItem(todo){
+        this.todos.unshift(todo)
+      }
+    }
+  }
+</script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
