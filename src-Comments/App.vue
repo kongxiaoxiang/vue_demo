dc2d3ca9404cb8a573f634e29b43469dea69aac0
mac <mac@192.168.2.3>
2020-10-21 14:49:37 +0800

    更新

---
+++ src-Comments/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments" :deleteItem='deleteItem'/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Add from './components/Add'
+  import List from './components/List'
+  export default {
+    data (){
+      return {
+        comments:[
+          {name:'bob',content:'vue good'},
+          {name:'jack',content:'vue easy'},
+          {name:'rose',content:'vue so so'},
+        ]
+      }
+    },
+    components:{
+      Add,
+      List
+    },
+    methods:{
+      addComment(comment){
+        this.comments.unshift(comment)
+      },
+      deleteItem(index){
+        this.comments.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
